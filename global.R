12a404c2ea184b13aa9e0f3e728b1d8bd2efb7fb
Ian <ianmseddy@gmail.com>
2025-03-27 09:21:58 -0700

    update global

--- global.R
+++ global.R
@@ -6,25 +6,18 @@ getOrUpdatePkg(c("Require", "SpaDES.project"), c("1.0.1.9003", "0.1.1.9009")) # 
 # Version should be 0.1.1.9009
 #remotes::install_github("PredictiveEcology/reproducible@prepInputsForMacZip2")
 
-
-
-# update this for wherever you have this project stored
-projPath = "caribouForecastSSUD_WBI"
-
+projPath <- getwd()
 
 out <- SpaDES.project::setupProject(
-  Restart = TRUE,
   useGit = TRUE,
-  updateRprofile = FALSE,
-  paths = list(projectPath = "caribouForecastSSUD_WBI"),
-  
+  name = "caribouForecastSSUD_WBI",
   modules = c("PredictiveEcology/Biomass_borealDataPrep@development",
               "PredictiveEcology/Biomass_core@main",
               "PredictiveEcology/Biomass_regeneration@development",
               "PredictiveEcology/Biomass_speciesParameters@development",
               "PredictiveEcology/scfm@development",
               #note scfm is a series of modules on a single git repository
-              'JWTurn/caribou_SSUD@master'
+              'JWTurn/caribou_SSUD@main'
               
   ),
   params = list(
@@ -35,11 +28,7 @@ out <- SpaDES.project::setupProject(
       .studyAreaName=  "caribouWBI_4maps",
       .useCache = c(".inputObjects", "init")
     ),
-    Biomass_speciesParamters = list("PSPdataTypes" = "dummy"),
-    scfmDataPrep = list(targetN = 2000,
-                        fireRegimePolysType = c("FRU"),
-                        # targetN would ideally be minimum 2000 - mean fire size estimates will be bad with 1000
-                        .useParallelFireRegimePolys = TRUE) #assumes parallelization is an option
+    Biomass_speciesParamters = list("PSPdataTypes" = "dummy")
   ),
   options = list(#spades.allowInitDuringSimInit = TRUE,
     spades.allowSequentialCaching = TRUE,
@@ -52,15 +41,13 @@ out <- SpaDES.project::setupProject(
   #70 years of fire should be enough to evaluate MAAB ## I'm currently testing
   studyArea = {
     sa <- reproducible::prepInputs(url = 'https://drive.google.com/file/d/1XduunieEoZLcNPQphGXnKG7Ql9MF1bme/view?usp=share_link',
-                                   destinaionPath = file.path(projPath, "data", "prepInputs"),
+                                   destinationPath = "inputs",
                                    targetFile = "studyArea_4maps.shp",
                                    alsoExtract = "similar", fun = "terra::vect")
   },
-  # my area is so large probably don't need saLarge and rastertomatchLarge ****
   studyAreaLarge = {
     terra::buffer(studyArea, 2000)
   },
-  # my area is so large probably don't need saLarge and rastertomatchLarge ****
   studyAreaLarge = sf::st_buffer(studyArea, 10000),
   
   rasterToMatchLarge = {
@@ -72,7 +59,6 @@ out <- SpaDES.project::setupProject(
     rtm <- reproducible::postProcess(rasterToMatchLarge, cropTo = studyArea, maskTo = studyArea)
   },
   sppEquiv = {
-    speciesInStudy <- LandR::speciesInStudyArea(studyAreaLarge)
     speciesInStudy <- LandR::speciesInStudyArea(studyAreaLarge, dPath = "inputs")
     
     species <- LandR::equivalentName(speciesInStudy$speciesList, df = LandR::sppEquivalencies_CA, "LandR")
@@ -81,5 +67,17 @@ out <- SpaDES.project::setupProject(
   }
 )
 
+out$modules <- c("Biomass_borealDataPrep", "Biomass_core",
+                 "Biomass_regeneration", "Biomass_speciesParameters",
+                 "scfmIgnition", "scfmEscape", "scfmSpread",
+                 "scfmDiagnostics", "scfmDataPrep",
+                 "caribou_SSUD")
+out$paths$modulePath <- c("modules", "modules/scfm/modules")
+out$params$scfmDataPrep = list(targetN = 2000,
+                               fireRegimePolysType = c("FRU"),
+                               # targetN would ideally be minimum 2000 - mean fire size estimates will be bad with 1000
+                               .useParallelFireRegimePolys = TRUE) #assumes parallelization is an option
+
+pkgload::load_all("../LandR") #while you wait for NTEMS function
 outSim <- SpaDES.core::simInitAndSpades2(out) |>
   reproducible::Cache()
